ba17b918ec31aea33e4810d7b22ca46c0da2234e
artemijr <arrtemijr@gmail.com>
2023-04-30 16:50:22 +0300

    fixed ExecuteScriptCommand

--- Server/src/main/kotlin/commands/ExecuteScriptCommand.kt
+++ Server/src/main/kotlin/commands/ExecuteScriptCommand.kt
@@ -1,36 +1,32 @@
 package commands
 
 import data.Messages
+import exeptions.CommandException
 import org.koin.core.component.inject
-import utils.CommandParser
+import utils.CommandExecutor
 import utils.Printer
 import java.io.File
 import java.io.FileNotFoundException
 import java.util.*
 
-/**
- * The ExecuteScriptCommand class reads and executes commands from the specified file.
- *
- * @property commandParser The command parser used to parse and execute commands.
- * @property printer The printer used to display command results.
- * @property nestedLevel The current script nesting level (default 0).
- */
 class ExecuteScriptCommand(
-    private val commandParser: CommandParser,
+    private val commandExecutor: CommandExecutor,
     private val printer: Printer,
     private val nestedLevel: Int = 0
 ) : Command() {
     val stack: Stack<String> by inject()
+    // Define the required arguments for this command
+    private val requiredArgs = listOf("fileName")
 
     fun copy(nestedLevel: Int): ExecuteScriptCommand {
-        return ExecuteScriptCommand(commandParser, printer, nestedLevel)
+        return ExecuteScriptCommand(commandExecutor, printer, nestedLevel)
     }
 
     override fun execute(args: List<Any>): String {
-
         val fileName = args[0] as String
         val file = File(fileName)
-        if(stack.contains(fileName)) return "Error: Recursion"
+        if (stack.contains(fileName)) return "Error: Recursion"
+
         return try {
             if (!file.exists()) {
                 throw FileNotFoundException(Messages.FILE_NOT_FOUND + fileName)
@@ -40,7 +36,7 @@ class ExecuteScriptCommand(
             while (lines.hasNext()) {
                 val line = lines.next().trim()
                 if (line.isNotBlank()) {
-                    val commandResult = commandParser.parseAndExecute(line, nestedLevel + 1, lines::next)
+                    val commandResult = executeLine(line, lines::next)
                     if (commandResult != null) {
                         printer.println(commandResult)
                     }
@@ -53,6 +49,39 @@ class ExecuteScriptCommand(
         }
     }
 
+    private fun executeLine(line: String, input: () -> String): String? {
+        val commandParts = line.split(" ", limit = 2)
+        val commandName = commandParts[0]
 
+        val command = commandExecutor.getCommand(commandName)
+            ?: throw CommandException("Unknown command: $commandName")
 
+        val initialArg = commandParts.getOrElse(1) { "" }
+        val args = readArguments(command, input, initialArg)
+
+        return when {
+            command is ExecuteScriptCommand -> {
+                command.copy(nestedLevel = nestedLevel + 1).execute(args)
+            }
+            line.isNotBlank() -> {
+                command.execute(args)
+            }
+            else -> null // Return null if the input is empty (ignores empty lines)
+        }
+    }
+
+    private fun readArguments(command: Command, input: (() -> String)?, initialArg: String): List<Any> {
+        val args = mutableListOf<Any>()
+        if (initialArg.isNotBlank()) {
+            args.add(initialArg)
+        }
+
+        // Read additional arguments if necessary
+        val requiredArgs = (command as? ExecuteScriptCommand)?.requiredArgs ?: emptyList()
+        while (args.size < requiredArgs.size) {
+            input?.invoke()?.let { arg -> args.add(arg) }
+        }
+
+        return args
+    }
 }
